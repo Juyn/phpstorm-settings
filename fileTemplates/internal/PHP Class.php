d483092b6022251bf0f934776e6d7810de716321
Xavier Dubois <x.dubois@laboutiqueofficielle.com>
2019-06-03 14:50:55 +0200

    PS-2019.1.2 <xavier@tardis Create baseRefactoring.xml, PHP File AuthorShip.php, PHP Class.php, ide.general.xml, _@user_Default.icls, composerJson.xml, _@user_MatteCarbon.icls, git_toolbox_2.xml, project.default.xml, rainbow_brackets.xml, Default copy.xml, Default.xml, ignore.xml, keymap.xml, find.xml, ui.lnf.xml, _@user_Nord.icls, log_highlighting.xml, filetypes.xml, php.xml, remote-servers.xml, colors.scheme.xml, External Tools.xml, _@user_Darcula.icls, PHP Trait.php, databaseDrivers.xml, _@user_Atom One Light.icls, stringManipulation.xml, GitHub.icls, React.xml, Material Peacock.icls, Default.xml, PHPUnit Test.php, material_custom_theme.xml, docker.xml, PHP Fluent Setter Method.php, _@user_LightCarbon.icls, laf.xml, PHP File Header.php, material_theme.xml, PHP Interface.php, editor.xml, usageView.xml, debugger.xml, Photon - Light.icls, PHP File.php, updates.xml

---
+++ fileTemplates/internal/PHP Class.php
@@ -0,0 +1,17 @@
+<?php
+
+#parse("PHP File Header.php")
+
+#if (${NAMESPACE})
+
+namespace ${NAMESPACE};
+
+#end
+/**
+  * Class ${NAME}
+  *
+#parse("PHP File AuthorShip.php")
+  */
+class ${NAME} {
+
+}
---
+++ fileTemplates/internal/PHP Trait.php
@@ -0,0 +1,16 @@
+<?php
+#parse("PHP File Header.php")
+
+#if (${NAMESPACE})
+
+namespace ${NAMESPACE};
+
+#end
+/**
+  * Class ${NAME}
+  *
+#parse("PHP File AuthorShip.php")
+  */
+trait ${NAME} {
+
+}
